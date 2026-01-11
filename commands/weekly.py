ffe38881ca71851430340568502ccf6b5dc7848b
kettleTeacues <teacues@gmail.com>
2026-01-11 16:37:01 +0900

    weeklyコマンドを実装

--- commands/weekly.py
+++ commands/weekly.py
@@ -1,15 +1,24 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import time
 import yfinance as yf
 
 from utils.jquants import JQuantsClient
-from insert_yf.stock_info import insert_stock_info
+from insert_yf.stock_balancesheet import insert_stock_balancesheet
 
 jq_client = JQuantsClient()
 companies = jq_client.getCompanies()
 symbols = [company.get('Code', '')[:4] for company in companies]
 
-for symbol in symbols[:1]:
+errors=[]
+for symbol in symbols:
     yf_client = yf.Ticker(f'{symbol}.T')
 
-    insert_stock_info(yf_client)
+    try:
+        insert_stock_balancesheet(yf_client, period='quarterly')
+    except Exception as e:
+        errors.append(f"Error inserting stock history for {symbol}: {e}")
+
     time.sleep(0.1)
+print(errors)
